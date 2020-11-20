4f6d9a7a248979e56887eee2820d7d6794a8df59
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2020-11-20 23:13:07 +0100

    Replaced char[][] with int[][]

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
             { 1, 0, 1 },
             { 1, 1, 1 }
     };
-    char symbol = '#';
+    char symbol = 'X';
 
     public static void main(String[] args) {
         var app = new MainApp();
@@ -16,32 +16,32 @@ public class MainApp {
         // einlesen des feldes
         // in dem array "feld" stehen "x" und " " wie in der eingabedatei
         //name der datei als uebergabenparameter eingeben
-        char[][] feld = getData("karte" + fileNumber + ".txt");
+        int[][] feld = getData("karte" + fileNumber + ".txt");
 
 
         // feld testweise ausgeben
         for (int i = 0; i < feld.length; i++) {
             for (int j = 0; j < feld[0].length; j++) {
-                System.out.print(feld[i][j]);
+                System.out.print(feld[i][j] == 1 ? "X" : " ");
             }
             System.out.println();
         }
     }
 
-    private char[][] getData(String fileName){
+    private int[][] getData(String fileName){
         String[] file = DataUtils.loadStrings(fileName);
         int columns  = Integer.parseInt(file[0]);
         int rows = Integer.parseInt(file[1]);
 
-
-        char[][] data = new char[rows][columns];
+        int[][] data = new int[rows][columns];
         for (int i = 0; i < rows; i++) {
-            data[i] = file[i+2].toCharArray();
+            char[] lineCharacters = file[i+2].toCharArray();
+            for(int j = 0; j < columns; j++) {
+                data[i][j] = lineCharacters[j] == symbol ? 1 : 0;
+            }
         }
 
         return data;
 
     }
-
-
 }
